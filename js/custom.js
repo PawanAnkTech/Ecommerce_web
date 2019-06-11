0fc5a533b39874a815c464b6370c035c699400ee
PawanAnkTech <pawan.kumar@anktech.co.in>
2019-06-11 18:21:25 +0530

    banner begain and sass and slick slider integrated

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    centerMode: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+ });
