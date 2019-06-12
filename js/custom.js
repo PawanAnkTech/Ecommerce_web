e04b4f8b40ced2c1ebb24748da3c1d4409fd1f91
PawanAnkTech <pawan.kumar@anktech.co.in>
2019-06-12 12:17:12 +0530

    banner completed

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,12 @@ $('.fade').slick({
     speed: 700,
     centerMode: true,
     fade: true,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     arrows: true,
     slidesToShow: 1,
     slidesToScroll: 1
  });
+
+   AOS.init();
+ 
